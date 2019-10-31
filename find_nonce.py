6975da1736db8def6f5b1a497b51709aa48b7e06
Adam Sadiq <adamsadiq@Adams-MBP-4.lan>
2019-10-31 19:02:11 +0000

    able to run file from another

--- find_nonce.py
+++ find_nonce.py
@@ -1,26 +1,21 @@
 import hashlib
 import time
-  
+import sys
+
+print("Program is being run")
+
+# -------------------------------------------------------- GLOBAL VARIABLES -------------------------------------------------------- #
 challenger_string = "COMSM0010cloud"
 pure_zeros = True
-# challenger_string = ' '.join(map(bin,bytearray(original_challenger_string,'utf8')))
-
-scale = 16 ## equals to hexadecimal
-num_of_bits = 256
-number_of_zeros = 0
-
-# def hexToBinary(hex_d):
-#     binary = hex(hex_d))
-#     for i in range (0,4):
-#         if
-#         number_of_zeros+=1
+scale = 16 # equals to hexadecimal
+num_of_bits = 256 #number of bits to output from hex
+# -------------------------------------------------------- GLOBAL VARIABLES -------------------------------------------------------- #
 
 def find_nonce(start, end, difficulty):
     for i in range(start, end):
         pure_zeros = True
         nonce_found = False
-        proof = "{0:b}".format(i) #no need to do this
-        # print(proof)
+        proof = "{0:b}".format(i)
         concat = proof + challenger_string
         result = hashlib.sha256(concat.encode('utf-8')).hexdigest()
         result_binary = bin(int(result, scale))[2:].zfill(num_of_bits)
@@ -33,19 +28,22 @@ def find_nonce(start, end, difficulty):
                 if (pure_zeros):
                     nonce_found = True
                     print("Nonce found:")
-                    # print(result_binary) #printing value
                     break
             j += 1
         if (nonce_found):
             break
+    return (result_binary)
 
-        # if result_binary[0:difficulty] & 111111111111111
-
-
-iterations = input("iterations:")
-difficulty = input("difficulty:")
 
 start = time.time() # countdown
-
-find_nonce(0, iterations, difficulty)
-print(str(time.time() - start) + "s")
+if (__name__ == "__main__"):
+    start_i = int(sys.argv[1])
+    end_i = int(sys.argv[2])
+    difficulty = int(sys.argv[3])
+    result = find_nonce(start_i, end_i, difficulty)
+    time = str(time.time() - start) + "s"
+    print(result)
+    print(time)
+    #stop all instances and return
+else:
+    print("Program ran with no arguments!")
