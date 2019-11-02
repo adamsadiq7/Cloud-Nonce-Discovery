e49b161d7645e587dffa39a57bedb6dbde954a29
Adam Sadiq <adamsadiq@Adams-MBP-4.lan>
2019-11-02 21:56:02 +0000

    fixed blockchain program

--- find_nonce.py
+++ find_nonce.py
@@ -2,19 +2,17 @@ import hashlib
 import time
 import sys
 
-print("Program is being run")
-
 # -------------------------------------------------------- GLOBAL VARIABLES -------------------------------------------------------- #
 challenger_string = "COMSM0010cloud"
 pure_zeros = True
 scale = 16 # equals to hexadecimal
 num_of_bits = 256 #number of bits to output from hex
+nonce_found = False
 # -------------------------------------------------------- GLOBAL VARIABLES -------------------------------------------------------- #
 
 def find_nonce(start, end, difficulty):
     for i in range(start, end):
         pure_zeros = True
-        nonce_found = False
         proof = "{0:b}".format(i)
         concat = proof + challenger_string
         result = hashlib.sha256(concat.encode('utf-8')).hexdigest()
@@ -28,11 +26,9 @@ def find_nonce(start, end, difficulty):
                 if (pure_zeros):
                     nonce_found = True
                     print("Nonce found:")
-                    break
+                    print(result_binary)
+                    return (result_binary)
             j += 1
-        if (nonce_found):
-            break
-    return (result_binary)
 
 
 start = time.time() #countdown
@@ -40,10 +36,11 @@ if (__name__ == "__main__"):
     start_i = int(sys.argv[1])
     end_i = int(sys.argv[2])
     difficulty = int(sys.argv[3])
-    result = find_nonce(start_i, end_i, difficulty)
     time = str(time.time() - start) + "s"
-    print(result)
-    print(time)
-    #stop all instances and return
+    result = find_nonce(start_i, end_i, difficulty)
+    if (result == None):
+        print("No nonce found")
+    else:
+        print(time)
 else:
     print("Program ran with no arguments!")
